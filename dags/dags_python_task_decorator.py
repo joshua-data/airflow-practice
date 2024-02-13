a897299c396e3b3c53afe311357425d801bb9b57
Joshua Kim <joshuakim@Joshuas-MacBook-Air.local>
2024-02-14 08:08:00 +0900

    task_decorator 오타 수정

--- dags/dags_python_task_decorator.py
+++ dags/dags_python_task_decorator.py
@@ -12,7 +12,7 @@ with DAG(
 ) as dag:
     
     @task(task_id='python_task_1')
-    def print_context(some_input);
+    def print_context(some_input):
         print(some_input)
     
     python_task_1 = print_context('task_decorator 실행')
