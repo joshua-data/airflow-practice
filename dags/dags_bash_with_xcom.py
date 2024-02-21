73f75f0d95b1eb3712c4fe4e7306d6e47edeec20
Joshua Kim <joshuakim@Joshuas-MacBook-Air.local>
2024-02-21 23:08:56 +0900

    dags_bash_with_xcom 오타 수정

--- dags/dags_bash_with_xcom.py
+++ dags/dags_bash_with_xcom.py
@@ -22,7 +22,7 @@ with DAG(
     bash_pull = BashOperator(
         task_id = 'bash_pull',
         env = {
-            'PUSHED_VALUE': "{{ ti.xcom_pull(key = 'bash_pushed) }}",
+            'PUSHED_VALUE': "{{ ti.xcom_pull(key = 'bash_pushed') }}",
             'RETURN_VALUE': "{{ ti.xcom_pull(task_id = 'bash_push') }}"
         },
         bash_command = "echo $PUSHED_VALUE && echo $RETURN_VALUE",
