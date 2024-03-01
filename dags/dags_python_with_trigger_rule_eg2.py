f725c53dcb324ab91f4cf41d8dc6e1992a643344
Joshua Kim <joshuakim@Joshuas-MacBook-Air.local>
2024-03-02 06:54:17 +0900

    dags_python_with_trigger_rule_eg2.py 오타 수정

--- dags/dags_python_with_trigger_rule_eg2.py
+++ dags/dags_python_with_trigger_rule_eg2.py
@@ -38,7 +38,7 @@ with DAG(
     def task_c():
         print('정상 처리')
 
-    @task(task_id = 'task_d', trigger_rule = 'non_skipped')
+    @task(task_id = 'task_d', trigger_rule = 'none_skipped')
     def task_d():
         print('정상 처리')
 
